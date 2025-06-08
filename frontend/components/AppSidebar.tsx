234fe8d09fca5afda6acfa53bbf29539185a6099
hydrologyrcrec <hydrologyrcrecflorida@gmail.com>
2025-06-07 20:37:48 -0400

    Update api url in other components

--- frontend/components/AppSidebar.tsx
+++ frontend/components/AppSidebar.tsx
@@ -68,10 +68,10 @@ export function AppSidebar() {
       const isVisible = updated[filename]
   
       if (filename === 'topography') {
-        const url = 'http://localhost:5008/tiffs/Top1_web.tif'
+        const url = `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:5008"}/tiffs/Top1_web.tif`
         isVisible ? addRasterLayer('topography', url) : removeRasterLayer('topography')
       } else if (filename === 'bedrock') {
-        const url = 'http://localhost:5008/tiffs/Bedrock1_web.tif'
+        const url = '${process.env.NEXT_PUBLIC_API_URL || "http://localhost:5008"}/tiffs/Bedrock1_web.tif'
         isVisible ? addRasterLayer('bedrock', url) : removeRasterLayer('bedrock')
       } else if (filename) {
         loadGeoJSON(filename, isVisible)
