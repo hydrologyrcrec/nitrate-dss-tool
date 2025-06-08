328b389bce5c522dfe72e91d2b2e311537e150ac
hydrologyrcrec <hydrologyrcrecflorida@gmail.com>
2025-06-08 03:43:00 -0400

    Integrate FSM into Dashboard

--- frontend/components/AppSidebar.tsx
+++ frontend/components/AppSidebar.tsx
@@ -16,11 +16,15 @@ import {
     CollapsibleContent,
     CollapsibleTrigger,
 } from "@radix-ui/react-collapsible";
-import { ChevronDown, Database, Map, ChartNoAxesCombined, Package, BrickWall, Waves, Layers, LoaderPinwheel, PenLine, Sparkle, MapPinned } from "lucide-react";
+import { ChevronDown, ChevronUp , Database, Map, ChartNoAxesCombined, Package, BrickWall, Waves, Layers, LoaderPinwheel, PenLine, Sparkle, MapPinned, Eye } from "lucide-react";
 import StationList from './StationList';
 import SWStationList from "./SwStationList";
 import { useStationContext } from "@/app/contexts/StationContext";
-import { icon } from 'leaflet';
+import dynamic from 'next/dynamic';
+
+const LeafletMapWithCOG = dynamic(() => import('@/components/LeafletMapWithCOG'), {
+  ssr: false,
+});
 
 const allGeoJSONLayers = {
   studyArea: [
@@ -58,9 +62,29 @@ const allGeoJSONLayers = {
   ],
 }
 
+const bmpHeaders = [
+  { title: 'Study Area', data: allGeoJSONLayers.studyArea, icon: <PenLine className="h-4 w-4"/> },
+  { title: 'Wells', data: allGeoJSONLayers.wells, icon: <BrickWall className="h-4 w-4"/>},
+  { title: 'Maps', data: allGeoJSONLayers.maps, icon: <Map className="h-4 w-4"/> },
+  { title: 'Model Setup', data: allGeoJSONLayers.modelSetup, icon: <Package className="h-4 w-4"/>},
+  { title: 'Model Results', data: allGeoJSONLayers.modelResults, icon: <Sparkle className="h-4 w-4"/>},
+  { title: 'Scenarios', data: allGeoJSONLayers.scenarios, icon: <LoaderPinwheel className="h-4 w-4"/>},
+]
+
+const fsmTiffFiles = [
+  {label: 'Drainage', file: 'Drainage_cog_v2.tif'},
+  {label: 'Flood Inventory', file: 'Flood_inventory_cog_v1.tif'},
+]
+
+interface fsmTF {
+  label: string; 
+  file: string
+}
+
 export function AppSidebar() {
     const { state, dispatch } = useStationContext();
     const [visibleLayers, setVisibleLayers] = useState<Record<string, boolean>>({})
+    const [visibleTiff, setVisibleTiff] = useState<string | null>(null);
 
     const toggleLayer = (filename: string) => {
       const updated = { ...visibleLayers, [filename]: !visibleLayers[filename] }
@@ -94,23 +118,59 @@ export function AppSidebar() {
         ))}
       </div>
     )
+
+    const renderTiffCheckboxes = (group: fsmTF[]) => (
+      <div className="space-y-1 pl-4 py-1">
+        {group.map(({ label, file }) => (
+          <div key={label} className="flex items-center gap-2 text-sm">
+            <input
+              type="checkbox"
+              checked={visibleTiff === file}
+              onChange={() => {
+                setVisibleTiff(visibleTiff === file ? null : file);
+              }}
+            />
+            {label}
+          </div>
+        ))}
+      </div>
+    );    
   
     return (
     <SidebarProvider className="z-30 fixed top-[60px] right-0 max-w-1/4 h-[100vh] overflow-scroll bg-white">
+        {visibleTiff && (
+          <div className="w-full h-screen fixed top-[60px] left-0 z-10">
+            <LeafletMapWithCOG file={visibleTiff} />
+          </div>
+        )}
       <Sidebar side="right" className="w-1/4 fixed top-[60px] right-0 overflow-scroll bg-white">
         <SidebarMenu className="pt-4 bg-white overflow-scroll h-screen">
             <Collapsible className="group/collapsible">
                 <SidebarMenuItem >
                     <CollapsibleTrigger className="flex items-center justify-between w-full text-lg font-semibold px-2 py-2 rounded-md hover:bg-muted/60 transition">
                     <span className="flex items-center justify-center gap-2"><MapPinned className="h-4 w-4" />Flood Susceptibility Mapping</span>
-                        <ChevronDown className="h-4 w-4 ml-2 transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                        <ChevronUp className="h-4 w-4 ml-2 transition-transform group-data-[state=open]/collapsible:rotate-180" />
                     </CollapsibleTrigger>
                     <CollapsibleContent>
-                        <SidebarMenuSub>
-                            <SidebarMenuSubItem>
-                                <SidebarMenuSubButton> <span className="text-sm text-gray-500 p-2"> Coming Soon </span></SidebarMenuSubButton>
-                            </SidebarMenuSubItem>
-                        </SidebarMenuSub>
+                        <SidebarMenu>
+                          <SidebarMenuItem>
+                            <SidebarMenuSub>
+                                <Collapsible className="group/collapsible">
+                                  <SidebarMenuSubItem>
+                                    <CollapsibleTrigger className="flex items-center justify-between w-full text-lg font-semibold px-2 pt-2 rounded-md hover:bg-muted/60 transition">
+                                        <span className="flex items-center justify-center gap-2"><Eye className="h-4 w-4" /><p className="text-sm">View</p></span>
+                                        <ChevronDown className="h-4 w-4 ml-2" />
+                                    </CollapsibleTrigger>
+                                    <SidebarMenuSub>
+                                    <CollapsibleContent className='max-h-screen overflow-scroll pt-2'>
+                                        {renderTiffCheckboxes(fsmTiffFiles)}
+                                    </CollapsibleContent>
+                                    </SidebarMenuSub>
+                                  </SidebarMenuSubItem>
+                                </Collapsible>
+                            </SidebarMenuSub>
+                            </SidebarMenuItem>
+                        </SidebarMenu>
                     </CollapsibleContent>
                 </SidebarMenuItem>
             </Collapsible>
@@ -119,7 +179,7 @@ export function AppSidebar() {
                 <SidebarMenuItem >
                     <CollapsibleTrigger className="flex items-center justify-between w-full text-lg font-semibold px-2 py-2 rounded-md hover:bg-muted/60 transition">
                     <span className="flex items-center justify-center gap-2"><Waves className="h-4 w-4" /> Surface & Ground Water</span>
-                        <ChevronDown className="h-4 w-4 ml-2 transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                        <ChevronUp className="h-4 w-4 ml-2 transition-transform group-data-[state=open]/collapsible:rotate-180" />
                     </CollapsibleTrigger>
                     <CollapsibleContent>
                         <SidebarMenu>
@@ -127,9 +187,9 @@ export function AppSidebar() {
                                 <SidebarMenuSub>
                                     <Collapsible className="group/collapsible" open={state.dataDisplayState}  onOpenChange={(open) => dispatch({ type: "TOGGLE_DATA_STATE", payload: open })}>
                                         <SidebarMenuSubItem>
-                                        <CollapsibleTrigger className="flex items-center justify-between w-full text-lg font-semibold px-2 pt-2 rounded-md hover:bg-muted/60 transition">
+                                        <CollapsibleTrigger className="flex items-center justify-between w-full text-lg font-semibold px-2 py-2 rounded-md hover:bg-muted/60 transition">
                                             <span className="flex items-center justify-center gap-2"><ChartNoAxesCombined className="h-4 w-4" /><p className="text-sm">AI Prediction Results</p></span>
-                                            <ChevronDown className="h-4 w-4 ml-2 transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                                            <ChevronDown className="h-4 w-4 ml-2" />
                                         </CollapsibleTrigger>
                                         <SidebarMenuSub>
                                         <CollapsibleContent className='max-h-screen overflow-scroll'>
@@ -140,9 +200,9 @@ export function AppSidebar() {
                                     </Collapsible>
                                     <Collapsible className="group/collapsible" open={state.resultsDisplayState} onOpenChange={(open) => dispatch({ type: "TOGGLE_RESULTS_STATE", payload: open })}>
                                         <SidebarMenuSubItem>
-                                        <CollapsibleTrigger className="flex items-center justify-between w-full text-lg font-semibold px-2 pt-2 rounded-md hover:bg-muted/60 transition">
+                                        <CollapsibleTrigger className="flex items-center justify-between w-full text-lg font-semibold px-2 py-2 rounded-md hover:bg-muted/60 transition">
                                             <span className="flex items-center justify-center gap-2"><Database className="h-4 w-4" /><p className="text-sm">AI Prediction Data</p></span>
-                                            <ChevronDown className="h-4 w-4 ml-2 transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                                            <ChevronDown className="h-4 w-4 ml-2" />
                                         </CollapsibleTrigger>
                                         <SidebarMenuSub>
                                         <CollapsibleContent className='max-h-screen overflow-scroll'>
@@ -160,33 +220,32 @@ export function AppSidebar() {
 
 
           {/* BMP Modeling Study */}
-          <Collapsible>
+          <Collapsible className="group/collapsible" >
             <SidebarMenuItem>
-              <CollapsibleTrigger className="flex justify-between items-center w-full text-lg font-semibold px-2 py-2">
+              <CollapsibleTrigger className="flex items-center justify-between w-full text-lg font-semibold px-2 py-2 rounded-md hover:bg-muted/60 transition">
                 <span className="flex items-center gap-2"><Layers className="h-4 w-4" />BMP Modeling Study</span>
-                <ChevronDown className="h-4 w-4" />
+                <ChevronUp className="h-4 w-4 ml-2 transition-transform group-data-[state=open]/collapsible:rotate-180" />
               </CollapsibleTrigger>
               <CollapsibleContent className='mb-20'>
-                <SidebarMenuSub>
-                  {[
-                    { title: 'Study Area', data: allGeoJSONLayers.studyArea, icon: <PenLine className="h-4 w-4"/> },
-                    { title: 'Wells', data: allGeoJSONLayers.wells, icon: <BrickWall className="h-4 w-4"/>},
-                    { title: 'Maps', data: allGeoJSONLayers.maps, icon: <Map className="h-4 w-4"/> },
-                    { title: 'Model Setup', data: allGeoJSONLayers.modelSetup, icon: <Package className="h-4 w-4"/>},
-                    { title: 'Model Results', data: allGeoJSONLayers.modelResults, icon: <Sparkle className="h-4 w-4"/>},
-                    { title: 'Scenarios', data: allGeoJSONLayers.scenarios, icon: <LoaderPinwheel className="h-4 w-4"/>},
-                  ].map(({ title, data, icon }) => (
-                    <Collapsible key={title}>
-                      <CollapsibleTrigger className="flex justify-between w-full text-sm px-2 py-2 font-semibold">
-                        <span className="flex items-center justify-center gap-2">{icon}<p className="text-sm">{title}</p></span>
-                        <ChevronDown className="h-4 w-4" />
-                      </CollapsibleTrigger>
-                      <CollapsibleContent>
-                        {renderCheckboxes(data)}
-                      </CollapsibleContent>
-                    </Collapsible>
-                  ))}
-                </SidebarMenuSub>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                      <SidebarMenuSub>
+                        {bmpHeaders.map(({ title, data, icon }) => (
+                          <Collapsible key={title}>
+                            <CollapsibleTrigger className="flex justify-between w-full text-sm px-2 py-2 font-semibold">
+                              <span className="flex items-center justify-center gap-2">{icon}<p className="text-sm">{title}</p></span>
+                              <ChevronDown className="h-4 w-4" />
+                            </CollapsibleTrigger>
+                            <SidebarMenuSub>
+                            <CollapsibleContent>
+                              {renderCheckboxes(data)}
+                            </CollapsibleContent>
+                            </SidebarMenuSub>
+                          </Collapsible>
+                        ))}
+                      </SidebarMenuSub>
+                    </SidebarMenuItem>
+                  </SidebarMenu>
               </CollapsibleContent>
             </SidebarMenuItem>
           </Collapsible>
--- frontend/components/LeafletMapWithCOG.tsx
+++ frontend/components/LeafletMapWithCOG.tsx
@@ -4,7 +4,7 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 import COGViewer from './CogTiffViewer';
 import 'leaflet/dist/leaflet.css';
 
-export default function LeafletMapWithCOG() {
+export default function LeafletMapWithCOG({file}: {file: string}) {
   return (
     <MapContainer
       center={[20, 0]}
@@ -15,7 +15,7 @@ export default function LeafletMapWithCOG() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&copy; OpenStreetMap contributors"
       />
-      <COGViewer url="https://fsm-geo-tiff-data.s3.us-east-2.amazonaws.com/Drainage_cog_v2.tif" />
+      <COGViewer url={`https://fsm-geo-tiff-data.s3.us-east-2.amazonaws.com/${file}`} />
     </MapContainer>
   );
 }
