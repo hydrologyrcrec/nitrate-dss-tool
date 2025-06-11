230b33d0924aae770edf76dcf333d5ee8900fc48
hydrologyrcrec <hydrologyrcrecflorida@gmail.com>
2025-06-11 06:46:20 -0400

    Reset website icon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,6 +2,13 @@
 import './globals.css'
 import { ReactNode } from 'react'
 
+export const metadata = {
+  title: "Range AI",
+  icons: {
+    icon: "/icon.ico", // This is required
+  },
+};
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
